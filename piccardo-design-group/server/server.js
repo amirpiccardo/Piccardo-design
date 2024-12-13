9dc74e86ea57f020b1c18be88aa1f379e9bcdcf2
amirpiccardo <amirpiccardo@gmail.com>
2024-12-13 15:04:57 +0100

    modifica cors

--- piccardo-design-group/server/server.js
+++ piccardo-design-group/server/server.js
@@ -7,7 +7,7 @@ const path = require("path");
 const app = express();
 
 const corsOptions = {
-  origin: 'https://piccardo-design-group.onrender.com',
+  origin: '*',
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 };
